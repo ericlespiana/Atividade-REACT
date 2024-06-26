bf91b8e190be7a8e06fa4050dae93779dd974491
Ericles Piana <pianaericles@gmail.com>
2024-06-26 14:47:42 -0300

    task 1 done

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function home() {
+  return (
+    <>
+        <h1 className='titulo'>HOME</h1>
+        <img src="https://i.pinimg.com/736x/d4/3c/c5/d43cc59bf4acd6595442e63ad29ee43e.jpg" alt="heaven" className='img'/>
+    </>
+  )
+}
+
+export default home;
